d5009832652ee4f423491575d8aa008bdcf2d2ba
devworks8 <devworks8@gmail.com>
2019-01-15 00:24:38 -0500

    Layered down the package framework.

---
+++ hydramessenger/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="HydraPMS",
+    version="0.0.1.0",
+    author="Christian Lachapelle",
+    author_email="lachapellec@gmail.com",
+    description="Hydra Proxy/Messenger Service",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Devworks8/hydramessenger.git",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=["deepmerge",
+                      "setuptools",
+                      "PyYAML",
+                      "pyzmq",
+                      "tornado",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Development Status :: 2 - Pre-Alpha",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Intended Audience :: Developers",
+    ],
+    scripts=['curve_keygen.py',
+             ],
+    entry_points={
+        'console_scripts': ['HydraPMS=HydraPMS.hydrapms:main'],
+    }
+)
